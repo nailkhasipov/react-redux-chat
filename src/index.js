1032f7de78795a6ad4ae726956f4023f213ebeb5
Nail Khasipov <nail.khasipov@gmail.com>
2018-01-18 13:49:26 +0500

    webpack, react, bootstrap

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+const username = prompt('Введите имя пользователя');
+const socket = new WebSocket('ws://localhost:9000');
+
+socket.onopen = () => {
+  socket.send(JSON.stringify({
+    type: 'ADD_USER',
+    name: username
+  }));
+};
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data)
+  switch (data.type) {
+    case 'USERS_LIST':
+      populateUsers(data.users);
+      break;
+    case 'ADD_MESSAGE':
+      document.querySelector('#message-list').innerHTML += `<li>${data.name}: ${data.message}</li>`;
+      break;
+    default:
+      break;
+  }
+};
+
+function populateUsers(users) {
+  document.querySelector('#users').innerHTML = '';
+  users.forEach(user => {
+    document.querySelector('#users').innerHTML += `<li>${user.name}</li>`;
+  });
+}
+
+function sendMessage() {
+  const message = document.querySelector('#message').value;
+  document.querySelector('#message-list').innerHTML += `<li>${username}: ${message}</li>`;
+  socket.send(JSON.stringify({
+    type: 'ADD_MESSAGE',
+    message: message,
+    name: username
+  }));
+}
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: `${APP_DIR}/index.js`,
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.js?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
