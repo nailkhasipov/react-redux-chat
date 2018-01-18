e7a87da1a8694b40adb2a0981bac3ac1e5bcf478
Nail Khasipov <nail.khasipov@gmail.com>
2018-01-18 17:39:44 +0500

    components draft

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-class App extends React.Component {
-  render () {
-    return <p>Hello!</p>;
-  }
+function App() {
+  return (
+    <div className='container-fluid'>
+      <Sidebar />
+      <div className='col-md-8'>
+        <MessageList />
+        <AddMessage />
+      </div>
+    </div>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="col-md-3 sidebar">
+      <ul></ul>
+    </div>
+  );
+}
+
+function MessageList() {
+  return <ul></ul>;
+}
+
+function AddMessage() {
+  return <div className='add-message'><input type="text" /></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+// import chat from './reducers';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
 
-const username = prompt('Введите имя пользователя');
-const socket = new WebSocket('ws://localhost:9000');
+// const username = prompt('Введите имя пользователя');
+// const socket = new WebSocket('ws://localhost:9000');
 
-socket.onopen = () => {
-  socket.send(JSON.stringify({
-    type: 'ADD_USER',
-    name: username
-  }));
-};
+// socket.onopen = () => {
+//   socket.send(JSON.stringify({
+//     type: 'ADD_USER',
+//     name: username
+//   }));
+// };
 
-socket.onmessage = (event) => {
-  const data = JSON.parse(event.data)
-  switch (data.type) {
-    case 'USERS_LIST':
-      populateUsers(data.users);
-      break;
-    case 'ADD_MESSAGE':
-      document.querySelector('#message-list').innerHTML += `<li>${data.name}: ${data.message}</li>`;
-      break;
-    default:
-      break;
-  }
-};
+// socket.onmessage = (event) => {
+//   const data = JSON.parse(event.data)
+//   switch (data.type) {
+//     case 'USERS_LIST':
+//       populateUsers(data.users);
+//       break;
+//     case 'ADD_MESSAGE':
+//       document.querySelector('#message-list').innerHTML += `<li>${data.name}: ${data.message}</li>`;
+//       break;
+//     default:
+//       break;
+//   }
+// };
 
-function populateUsers(users) {
-  document.querySelector('#users').innerHTML = '';
-  users.forEach(user => {
-    document.querySelector('#users').innerHTML += `<li>${user.name}</li>`;
-  });
-}
+// function populateUsers(users) {
+//   document.querySelector('#users').innerHTML = '';
+//   users.forEach(user => {
+//     document.querySelector('#users').innerHTML += `<li>${user.name}</li>`;
+//   });
+// }
 
-function sendMessage() {
-  const message = document.querySelector('#message').value;
-  document.querySelector('#message-list').innerHTML += `<li>${username}: ${message}</li>`;
-  socket.send(JSON.stringify({
-    type: 'ADD_MESSAGE',
-    message: message,
-    name: username
-  }));
-}
+// function sendMessage() {
+//   const message = document.querySelector('#message').value;
+//   document.querySelector('#message-list').innerHTML += `<li>${username}: ${message}</li>`;
+//   socket.send(JSON.stringify({
+//     type: 'ADD_MESSAGE',
+//     message: message,
+//     name: username
+//   }));
+// }
